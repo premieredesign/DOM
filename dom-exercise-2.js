e3b006f510202020d6811a05dd9d5859d435af4a
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2020-03-05 13:06:17 -0500

    Updates
    in class exercise has Grace and Janna's code
     Exercise-1 is done
    Exercise-2 is almost there
    I don't know why the sort function won't work.

---
+++ dom-exercise-2.js
@@ -0,0 +1,43 @@
+// variable for button
+const addFood = document.querySelector('#submit');
+// variable for input
+const foodInput = document.querySelector('#food-input');
+
+function fruitSort() {
+	// variable for input value
+	let item = foodInput.value;
+	// convert input value into a text node
+	let text = document.createTextNode(item);
+	// create li element
+	let newItem = document.createElement('li');
+	// add text into li
+	newItem.appendChild(text);
+
+	let radioValue = () => {
+		choice = document.getElementsByName('fruitOrVeggies');
+
+		for (i of choice) {
+			if (i.checked) {
+				return i.value;
+			}
+		}
+	};
+	// *** This works
+	// if (radioValue() === 'fruit') {
+	// 	console.log(`GOT IT!!!`);
+	// } else {
+	// 	console.log(`NOPE`);
+	// }
+
+	// *** this does not
+	// add newItem to the ol
+	if (radioValue() === 'fruit') {
+		document.querySelector('#fruits').appendChild(newItem);
+	} else {
+	}
+	document.querySelector('#veggies').appendChild(newItem);
+
+	foodInput.value = '';
+}
+
+addFood.addEventListener('click', fruitSort);
