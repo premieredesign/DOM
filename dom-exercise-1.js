c4dbf8aa1313dfb2faf3064e1c189fb063dc8fa8
Ankur-1980 <58996699+Ankur-1980@users.noreply.github.com>
2020-03-04 22:54:26 -0500

    Initial commit

---
+++ dom-exercise-1.js
@@ -0,0 +1,18 @@
+/*
+Create and link a JavaScript file. 
+
+Create a function named “main”. The HTML is already set up to
+call this function when the “Action!!!” button is clicked.
+Within the main function:
+● Add the "big" class to the "grow-me" paragraph.
+● Remove the "big" class to the "shrink-me" paragraph.
+● Find all the <li>s and log their text content to the console.
+● Set the href of the link to "https://www.example.com" and update the text to say
+"somewhere" instead of "nowhere".
+● Set the "display" CSS property of the "hide-me" paragraph to "none".
+● Set the "display" CSS property of the "show-me" paragraph to "block".
+● Get the text that the user enters into the "name" input box and use it to set a welcome
+message in the <h1>, e.g., "Hello Grant!".
+*/
+
+let main = () => {};
---
+++ dom-exercise.js
@@ -0,0 +1,92 @@
+/*
+1. Here is a sample html file with a submit button. Now modify the style of the paragraph text through javascript code.
+- NOTE: Paste snippet below in Html body tag
+
+*/
+para = document.querySelector('#text');
+button = document.querySelector('#jsstyle');
+
+const changeStyle = () => {
+	para.style.fontSize = '25px';
+	para.style.color = 'purple';
+	para.style.backgroundColor = 'black';
+};
+
+button.addEventListener('click', changeStyle);
+
+/*
+Write a javascript function to get the values of First and Last name of the following form:
+ */
+
+//  creating a generic variable for the form
+const form1 = document.querySelector('#form1');
+
+// function to return the full name
+let fullName = e => {
+	// variables for first and last names
+	const firstName = document.getElementById('form1').elements.namedItem('fname').value;
+	const lastName = document.getElementById('form1').elements.namedItem('lname').value;
+
+	console.log(firstName, lastName);
+	e.preventDefault();
+};
+
+// creating the event listener
+form1.addEventListener('submit', fullName);
+
+/*
+3. Write a javascript function to set the background color of a paragraph.
+
+*/
+
+// creating a generic variable for the other paragraph
+otherPara = document.querySelector('#w3r');
+
+// function to resize the paragraph
+let resize = () => {
+	otherPara.style.fontSize = '45px';
+	otherPara.style.backgroundColor = 'tan';
+};
+
+// creating the event listener
+otherPara.addEventListener('mouseenter', resize);
+
+let bodyChange = document.querySelector('#body');
+
+let change = () => {
+	bodyChange.style.backgroundColor = 'tan';
+};
+
+bodyChange.addEventListener('load', change);
+
+/*
+4. Here is a sample html file with a submit button. Write a javascript function to get the value of the href, hreflang, rel, target, and type attributes of the specified link.
+
+*/
+
+let getAtt = document.querySelector('.attributesButton');
+let w3r = document.querySelector('#w3r');
+
+let hrefVal = w3r.getAttribute('href');
+let hreflangVal = w3r.getAttribute('hreflang');
+let relVal = w3r.getAttribute('rel');
+let targetVal = w3r.getAttribute('target');
+let typeVal = w3r.getAttribute('type');
+
+let getAttributes = () => {
+	console.log(hrefVal, hreflangVal, relVal, targetVal, typeVal);
+};
+
+getAtt.addEventListener('click', getAttributes);
+
+/*
+5. Write a javascript function to get the width and height of the window (any time the window is resized)
+
+*/
+// const button = document.createElement()
+const windowSize = () => {
+	console.log(`The height of the window is ${window.innerHeight}\n
+    The width of the window is ${window.innerWidth}`);
+};
+
+windowSize();
